fe10a0aac28c7cb9a42aeae8076622cae1834f73
wanosuke <contact@wanosuke.jp>
2025-03-09 18:46:45 +0900

    ver 0.9

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@ $("#js-drawer-icon").on("click", function () {
   $(this).toggleClass("is-opened");
   $("#js-drawer-content").toggleClass("is-opened");
 });
+
+// ページトップボタン
+const pageTop = document.querySelector("#js-pagetop");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    pageTop.classList.add("is-show");
+  } else {
+    pageTop.classList.remove("is-show");
+  }
+});
+
+// スマホのドロワーアイコンを消す処理
+$('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  $("#js-drawer-icon").removeClass("is-opened");
+  $("#js-drawer-content").removeClass("is-opened");
+});
